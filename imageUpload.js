d7a90153d4f6180e277d3817771bb06cbce95c25
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-01-27 18:20:32 +0530

    Update
    Image upload express route created

---
+++ imageUpload.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+const processhandler = require("./processhandler");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "media/photos");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.originalname.split(".")[0].toString() +
+        "-" +
+        uniqueSuffix +
+        `.${file.mimetype.split("/")[1].toString()}`
+    );
+  },
+});
+const upload = multer({ storage: storage }).array("files");
+
+router.post("/", (req, res) => {
+  try {
+    upload(req, res, (err) => {
+      if (err instanceof multer.MulterError) {
+        res.json({
+          ...processhandler?.returnJSONfailure,
+          msg: `Error: ${err}`,
+        });
+      } else if (err) {
+        res.json({
+          ...processhandler?.returnJSONfailure,
+          msg: `Something went wrong: ${err}`,
+        });
+      }
+      let returnArr = req.files.map((i) => {
+        return { ...i, path: i?.path?.replaceAll("\\", "/") };
+      });
+      res.json({
+        ...processhandler?.returnJSONsuccess,
+        returnData: returnArr,
+        msg: "Files uploaded successfully!",
+      });
+    });
+  } catch (error) {
+    res.json({
+      ...processhandler?.returnJSONfailure,
+      msg: `Something went wrong: ${error}`,
+    });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 const express = require("express");
-const dotenv = require('dotenv');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const processhandler = require('./processhandler');
-const utilfunctions = require('./utilfunctions');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const processhandler = require("./processhandler");
+const utilfunctions = require("./utilfunctions");
+const imageUpload = require("./imageUpload");
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(express.json());
 app.use(cors());
 
 dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 mongoose
-.connect(process.env.DB_URL)
-.then(() => console.log("Database Connected"))
-.catch((err) => console.log("error in DB", err));
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("error in DB", err));
 
-app.post('/', async (req, res) => {
-    let json = {}
-    if(!utilfunctions?.voidCheck(req?.body?.processId)){
-        json = {...processhandler?.returnJSONfailure, msg: 'Invalid request format, Process ID missing!'}
-    }
-    else{
-        json = await processhandler?.ProcessIdHandler(req?.body?.processId, req?.body?.datajson)
-    }
-    res?.send(json)
-})
+app.use("/imageUpload", imageUpload);
+
+app.post("/", async (req, res) => {
+  let json = {};
+  if (!utilfunctions?.voidCheck(req?.body?.processId)) {
+    json = {
+      ...processhandler?.returnJSONfailure,
+      msg: "Invalid request format, Process ID missing!",
+    };
+  } else {
+    json = await processhandler?.ProcessIdHandler(
+      req?.body?.processId,
+      req?.body?.datajson
+    );
+  }
+  res?.send(json);
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
