bdcf8077a2866a61aba80d3b10bf6cf451e849b7
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-02-07 23:47:34 +0530

    Update imageUpload.js

--- imageUpload.js
+++ imageUpload.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 const processhandler = require("./processhandler");
-const fs = require("node:fs");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
