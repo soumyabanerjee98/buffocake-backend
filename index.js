f49f2c7a31c90882d22fb146b2c2adb7320a057f
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-02-07 15:26:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const imageUpload = require("./imageUpload");
 const app = express();
 app.set("trust proxy", true);
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 dotenv.config();
 
